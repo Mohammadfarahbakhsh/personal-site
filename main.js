6a0063e2c133f455a8fdbf569be72d7abb3742a7
kazem <mohammadkazemfarahbakhsh5@gmail.com>
2025-04-04 00:49:36 +0330

    new update, i think its getting better

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 document.getElementById('mybutton').addEventListener('click', function() {
     alert('فعلاا چیزی نداریم');
 });
+document.getElementById('subject-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    if(name===""||email===""||message===""){
+        alert('لطفا پر نمایید')
+    }
+    else{
+        console.log('',name)
+        console.log('',email)
+        console.log('',message)
+        alert('با تشکر از شما')
+    }
+});
